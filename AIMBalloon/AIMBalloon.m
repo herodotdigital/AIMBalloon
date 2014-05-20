b88c4028677f6bb7fb04768e9af403602a27c169
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-05-20 23:16:19 +0200

    fixed performance issues && little cleanup

--- AIMBalloon/AIMBalloon.m
+++ AIMBalloon/AIMBalloon.m
@@ -37,11 +37,24 @@
 #error "AIMBalloon uses features only available in iOS SDK 7.0 and later."
 #endif
 
+NSInteger const kAIMBalloonNumberOfLinks = 10;
+CGFloat const kAIMBalloonSpaceBetweenLinks = 1.0f;
+CGSize const kAIMBalloonLinkSize = {10, 10};
+BOOL const kAIMBalloonDebug = NO;
+
 @interface AIMBalloon (){
     UIDynamicAnimator       *animator;
     UIView                  *balloon;
     UIAttachmentBehavior    *touchAttachmentBehavior;
+    
+    CAShapeLayer            *shapeLayer;
+    NSMutableArray          *linksArray;
+    NSMutableArray          *points;
+    
+    CADisplayLink           *displayLink;
 }
+
+@property (nonatomic, weak) UIView *linkedView;
 
 @end
 
@@ -49,103 +62,125 @@
 
 - (instancetype)initWithFrame:(CGRect)frame linkedToView:(UIView*)linkedView{
     if (self = [super initWithFrame:frame]){
+        self.linkedView = linkedView;
         
-        animator = [[UIDynamicAnimator alloc] initWithReferenceView:self];
-        UIView *previousLink        = linkedView;
-        
-        // setup variables
-        NSInteger numberOfLinks     = 10;
-        CGFloat spaceBetweenLinks   = 2.0f;
-        CGSize linkSize             = CGSizeMake(10, 10);
-        
-        // setup line parameters
-        CAShapeLayer *shapeLayer = [CAShapeLayer layer];
-        shapeLayer.fillColor = [[UIColor clearColor] CGColor];
-        shapeLayer.lineJoin = kCALineJoinRound;
-        shapeLayer.lineWidth = 1.0f;
-        shapeLayer.strokeColor = [UIColor redColor].CGColor;
-        shapeLayer.strokeEnd = 1.0f;
-        
-        [self.layer addSublayer:shapeLayer];
-        
-        NSMutableArray *linksArray      = [[NSMutableArray alloc] initWithCapacity:numberOfLinks];
-        NSMutableArray *points          = [[NSMutableArray alloc] initWithCapacity:numberOfLinks + 2];
-        
-        [points addObject:[NSValue valueWithCGPoint:previousLink.center]];
-        
-        CGFloat currentY = linkedView.frame.origin.y + linkedView.bounds.size.height;
+        [self setupAnimator];
+        [self setupShapeLayer];
+        [self setupLinks];
+        [self setupBalloon];
+        [self setupBehaviors];
+        [self setupDisplayLink];
 
-        // create links
-        for (int i = 0; i < numberOfLinks; i++) {
-            UIView *link = [[UIView alloc] initWithFrame:CGRectMake(self.bounds.size.width/2 - linkSize.width/2, currentY + spaceBetweenLinks, linkSize.width, linkSize.height)];
-            link.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.0];  // debug value
-            link.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
-            
-            [self addSubview:link];
-            
-            
-            UIAttachmentBehavior *attachmentBehavior = nil;
-            if (i == 0) {
-                attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:link
-                                                               offsetFromCenter:UIOffsetMake(0, 0)
-                                                               attachedToAnchor:linkedView.center];
-                
-            } else {
-                attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:link
-                                                               offsetFromCenter:UIOffsetMake(0, -1)
-                                                                 attachedToItem:previousLink
-                                                               offsetFromCenter:UIOffsetMake(0, 1)];
-            }
-            
-            [points addObject:[NSValue valueWithCGPoint:link.center]];
-            
-            [attachmentBehavior setAction:^{
-                points[i+1] = [NSValue valueWithCGPoint:link.center];
-                shapeLayer.path = [[UIBezierPath smoothPathFromArray:points] CGPath];
-            }];
-            
-            [animator addBehavior:attachmentBehavior];
-            
-            currentY += linkSize.height + spaceBetweenLinks;
-            [linksArray addObject:link];
-            previousLink = link;
-        }
-        
-        // create ballon
-        CGSize ballSize = CGSizeMake(60, 60);
-        balloon = [[UIView alloc] initWithFrame:CGRectMake(self.bounds.size.width/2 - ballSize.width/2, currentY + spaceBetweenLinks, ballSize.width, ballSize.height)];
-        balloon.backgroundColor = [UIColor greenColor];
-        balloon.layer.cornerRadius = ballSize.width/2;
-        [self addSubview:balloon];
-
-        
-
-        // Connect balloon to the chain
-        UIAttachmentBehavior *attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:previousLink
-                                                                               attachedToItem:balloon];
-        [animator addBehavior:attachmentBehavior];
-        
-        [points addObject:[NSValue valueWithCGPoint:balloon.center]];
-        [attachmentBehavior setAction:^{
-            points[numberOfLinks+1] = [NSValue valueWithCGPoint:balloon.center];
-            shapeLayer.path = [[UIBezierPath smoothPathFromArray:points] CGPath];
-        }];
-        
-        // Apply gravity and collision
-        [linksArray addObject:balloon];
-        UIGravityBehavior *gravityBeahvior = [[UIGravityBehavior alloc] initWithItems:linksArray];
-        [gravityBeahvior setGravityDirection:CGVectorMake(0, -1)];
-        [gravityBeahvior setMagnitude:0.5f];
-        UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:linksArray];
-        collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
-        [animator addBehavior:gravityBeahvior];
-        [animator addBehavior:collisionBehavior];
-        
     }
     return self;
 }
 
-// Let the user pull the balloon around
+#pragma mark - setup
+
+- (void)setupAnimator{
+    animator = [[UIDynamicAnimator alloc] initWithReferenceView:self];
+}
+
+- (void)setupShapeLayer{
+    shapeLayer = [CAShapeLayer layer];
+    shapeLayer.fillColor = [[UIColor clearColor] CGColor];
+    shapeLayer.lineJoin = kCALineJoinRound;
+    shapeLayer.lineWidth = 1.0f;
+    shapeLayer.strokeColor = [UIColor redColor].CGColor;
+    shapeLayer.strokeEnd = 1.0f;
+    [self.layer addSublayer:shapeLayer];
+}
+
+- (void)setupLinks{
+    UIView *previousLink = self.linkedView;
+    linksArray = [[NSMutableArray alloc] init];
+    points = [[NSMutableArray alloc] init];
+    [points addObject:[NSValue valueWithCGPoint:previousLink.center]];
+    CGFloat currentY = self.linkedView.frame.origin.y + self.linkedView.bounds.size.height;
+    
+    for (int i = 0; i < kAIMBalloonNumberOfLinks; i++) {
+        UIView *link = [[UIView alloc] initWithFrame:CGRectMake(self.bounds.size.width/2 - kAIMBalloonLinkSize.width/2,
+                                                                currentY + kAIMBalloonSpaceBetweenLinks,
+                                                                kAIMBalloonLinkSize.width,
+                                                                kAIMBalloonLinkSize.height)];
+        link.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:kAIMBalloonDebug ? 0.3f : 0.0f];  // debug value
+        link.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
+        
+        [self addSubview:link];
+        
+        
+        UIAttachmentBehavior *attachmentBehavior = nil;
+        if (i == 0) {
+            attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:link
+                                                           offsetFromCenter:UIOffsetMake(0, 0)
+                                                           attachedToAnchor:self.linkedView.center];
+            
+        } else {
+            attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:link
+                                                           offsetFromCenter:UIOffsetMake(0, -1)
+                                                             attachedToItem:previousLink
+                                                           offsetFromCenter:UIOffsetMake(0, 1)];
+        }
+        
+        [points addObject:[NSValue valueWithCGPoint:link.center]];
+        
+        [attachmentBehavior setAction:^{
+            points[i+1] = [NSValue valueWithCGPoint:link.center];
+        }];
+        
+        [animator addBehavior:attachmentBehavior];
+        
+        currentY += kAIMBalloonLinkSize.height + kAIMBalloonSpaceBetweenLinks;
+        [linksArray addObject:link];
+        previousLink = link;
+    }
+
+}
+
+- (void)setupBalloon{
+    CGSize ballSize = CGSizeMake(60, 60);
+    balloon = [[UIView alloc] initWithFrame:CGRectMake(self.bounds.size.width/2 - ballSize.width/2,
+                                                       kAIMBalloonNumberOfLinks * (kAIMBalloonLinkSize.height + kAIMBalloonSpaceBetweenLinks)
+                                                       + CGRectGetMaxY(self.linkedView.frame) + kAIMBalloonSpaceBetweenLinks,
+                                                       ballSize.width,
+                                                       ballSize.height)];
+    balloon.backgroundColor = [UIColor greenColor];
+    balloon.layer.cornerRadius = ballSize.width/2;
+    [self addSubview:balloon];
+    [points addObject:[NSValue valueWithCGPoint:balloon.center]];
+}
+
+- (void)setupBehaviors{
+    UIAttachmentBehavior *attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:[linksArray lastObject]
+                                                                           attachedToItem:balloon];
+    [animator addBehavior:attachmentBehavior];
+    
+    [attachmentBehavior setAction:^{
+        points[kAIMBalloonNumberOfLinks+1] = [NSValue valueWithCGPoint:balloon.center];
+    }];
+    
+    [linksArray addObject:balloon];
+    UIGravityBehavior *gravityBeahvior = [[UIGravityBehavior alloc] initWithItems:linksArray];
+    [gravityBeahvior setGravityDirection:CGVectorMake(0, -1)];
+    [gravityBeahvior setMagnitude:0.5f];
+    UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:linksArray];
+    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    [animator addBehavior:gravityBeahvior];
+    [animator addBehavior:collisionBehavior];
+}
+
+- (void)setupDisplayLink{
+    displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayView)];
+    [displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+}
+
+- (void)displayView{
+    shapeLayer.path = [[UIBezierPath smoothPathFromArray:points] CGPath];
+}
+
+
+#pragma mark - touches
+
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
     touchAttachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:balloon
                                                         attachedToAnchor:balloon.center];
